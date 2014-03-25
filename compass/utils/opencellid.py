255debc71e1e190af507b1728d84c85d1703fa84
Raghul Jagannathan <raghul@redmart.com>
2014-03-25 20:54:59 +0800

    url lib changes

--- compass/utils/opencellid.py
+++ compass/utils/opencellid.py
@@ -2,28 +2,21 @@ from xml.dom import minidom
 from xml.dom.minidom import parseString
 from urllib.parse import urlencode
 import urllib
+import urllib.request
 
 # make a string with the request type in it:
 method = "POST"
 url = "http://www.opencellid.org/cell/getMeasures?key=b33b3a39-a4ba-40f5-9ce0-8ba6e34dbba2"
-# create a handler. you can specify different handlers here (file uploads etc)
-# but we go for the default
-handler = urllib.request.HTTPHandler()
-# create an openerdirector instance
-opener = urllib.request.build_opener(handler)
-# build a request
-user_agent = 'Mozilla/5.0 (compatible; Chrome/22.0.1229.94; Windows NT)'
 # &mcc=260&mnc=2&lac=10250&cellid=26511&limit=10&offset=1
 
 
 def get_coordinates(mcc, mnc,lac, cellid):
     data_params = urllib.parse.urlencode({'mcc' : mcc, 'mnc'  : mnc, 'lac':lac, 'cellid':cellid, 'limit' :'10'})
     binary_data = data_params.encode('utf-8')
-    request = urllib.request.Request(url, data=binary_data )
-    request.get_method = lambda: method
+    request = urllib.request.Request(url)
     # try it; don't forget to catch the result
     #try:
-    connection = opener.open(request)
+    connection = urllib.request.urlopen(request, binary_data)
     #except urllib2.HTTPError,e:
     #connection = e
 
